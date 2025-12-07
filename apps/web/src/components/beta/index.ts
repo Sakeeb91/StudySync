6b123c9a5f02e2ff0980496ce3499f07509ecf28
Sakeeb91 <rahman.sakeeb@gmail.com>
2025-12-07 11:21:45 -0500

    feat(web): add beta components barrel export (SSC-12)
    Create index file exporting all beta components:
    - FeedbackForm, FloatingFeedbackButton
    - NPSSurvey, useNPSSurvey
    - useAnalytics, PageViewTracker, withClickTracking

---
+++ apps/web/src/components/beta/index.ts
@@ -0,0 +1,7 @@
+export { FeedbackForm, FloatingFeedbackButton } from "./FeedbackForm";
+export { NPSSurvey, useNPSSurvey } from "./NPSSurvey";
+export {
+  useAnalytics,
+  PageViewTracker,
+  withClickTracking,
+} from "./useAnalytics";
